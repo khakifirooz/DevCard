6fc3134c1d17b2b0ee53ed2c9b9c858c4b752206
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-12-18 13:16:00 +0330

    use partial model for projectsModel

--- DevCard_MVC/Models/Project.cs
+++ DevCard_MVC/Models/Project.cs
@@ -7,13 +7,15 @@ namespace DevCard_MVC.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Image { get; set; }
         public string Client { get; set; }
 
-        public Project(int id, string name, string description, string client)
+        public Project(int id, string name, string description, string image, string client)
         {
             Id = id;
             Name = name;
             Description = description;
+            Image = image;
             Client = client;
         }
     }
--- DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
+++ DevCard_MVC/ViewComponents/ProjectsViewComponent.cs
@@ -9,10 +9,10 @@ namespace DevCard_MVC.ViewComponents
         {
             var projects = new List<Project>
             {
-                new Project(1,"تاکسی", "درخواست تاکسی آنلاین برای سفر های درون شهری", "Snapp"),
-                new Project(2,"زود فود", "درخواست غذا آنلاین برای سراسر کشور", "Zood food"),
-                new Project(3,"مدارس", "سیسستم یکپارچه مدیریت مدارس ", "Monop"),
-                new Project(4,"فضاپیما", "سیستم مدیریت فضاپیما های ناسا ", "Nasa"),
+                new Project(1,"تاکسی", "درخواست تاکسی آنلاین برای سفر های درون شهری","project-1.jpg","Snapp"),
+                new Project(2,"زود فود", "درخواست غذا آنلاین برای سراسر کشور","project-2.jpg", "Zood food"),
+                new Project(3,"مدارس", "سیسستم یکپارچه مدیریت مدارس ","project-3.jpg", "Monop"),
+                new Project(4,"فضاپیما", "سیستم مدیریت فضاپیما های ناسا ","project-4.jpg", "Nasa"),
 
             };
             return View("_Projects", projects);
