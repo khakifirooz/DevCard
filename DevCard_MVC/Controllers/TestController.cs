2b6f5b6e4f87883434c9c8c04a648c7db70bdecf
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-12-22 15:53:58 +0330

    action result learning ( just a test )

---
+++ DevCard_MVC/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevCard_MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: TestController
+        public IActionResult Index()
+        {
+          //  return new StatusCodeResult(896);
+          //return new BadRequestResult();
+          return new OkResult();
+        }
+
+        //public RedirectToActionResult Index()
+        //{
+        //    return RedirectToAction("Contact", "Home");
+        //}
+    }
+}
