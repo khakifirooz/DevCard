e7cd10368d61a1d1a13608ea3e7c3c6a190ccd45
mehrshad <mehrshadkhakifirooz@gmail.com>
2025-12-23 19:23:02 +0330

    contact form client side validation implemented

--- DevCard_MVC/Models/Contact.cs
+++ DevCard_MVC/Models/Contact.cs
@@ -1,8 +1,17 @@
-﻿namespace DevCard_MVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json.Serialization;
+
+namespace DevCard_MVC.Models
 {
     public class Contact
     {
+        [Required(ErrorMessage ="این فیلد اجباری است")]
+        [MinLength(3,ErrorMessage ="حداقل اسم باید سه حرفی باشد")]
+        [MaxLength(20,ErrorMessage = "حداکثر اسم باید 20 حرفی باشد")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "این فیلد اجباری است")]
+        [EmailAddress(ErrorMessage ="مقدار وراد شده ایمیل صحیح نمیباشد")]
         public string Email { get; set; }
         public string Message { get; set; }
         public string Service { get; set; }
