c09357b665ce63da7165bab1618ee3d68a578b96
mehrshad <mehrshadkhakifirooz@gmail.com>
2026-07-18 12:02:48 +0330

    application ef add

---
+++ EFCore-Usecase-Project/EFCore_Application/ProductCategoryApplication.cs
@@ -0,0 +1,41 @@
+﻿using EFCore.Domain.ProductCategoryAgg;
+using EFCore_Application.Contract.ProductCategory;
+using EFCore_Infrasturactures.EfCore.Repository;
+
+namespace EFCore_Application
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+        
+        private readonly IProductCategoryRepository productCategoryRepository;
+
+        public ProductCategoryApplication(IProductCategoryRepository productCategoryRepository)
+        {
+            this.productCategoryRepository = productCategoryRepository;
+        }
+        public void Create(CreateProductCategory command)
+        {
+            if (productCategoryRepository.Exist(command.Name))
+                return;
+
+            var productCategory = new ProductCategory(command.Name);
+            productCategoryRepository.Create(productCategory);
+            productCategoryRepository.SaveChanges();
+        }
+
+        public void Edite(EditProductCategory command)
+        {
+            var productCategory = productCategoryRepository.Get(command.Id);
+            if (productCategory == null)
+                return;
+
+            productCategory.Edit(command.Name);
+            productCategoryRepository.SaveChanges();
+        }
+
+        public List<ProductCategoryViewModel> Search(string name)
+        {
+            return productCategoryRepository.GetAll(name);
+        }
+    }
+}
